19a0966e9fae54413ea2066d0631854f8a7f0ea6
Josh Reighley <josh@reighley.net>
2019-07-08 10:21:24 -0700

    Added listener loops to startup

--- src/clj_tello/core.clj
+++ src/clj_tello/core.clj
@@ -2,9 +2,14 @@
   (:require [clj-tello.udp :as drone]
             [mount.core :refer [defstate] :as mount]
             [clojure.string :as s]))
-(declare drone-send)
+(declare drone-send tello-command-socket start-tello)
 (def tello-address "192.168.10.1")
 (def tello-state (atom {}))
+
+(defn command "Transmits a command to Tello via UDP"
+  [command]
+  (drone/send tello-command-socket (str "command") tello-address 8889)
+  (drone/send tello-command-socket command tello-address 8889))
 
 (defstate tello-command-socket :start (drone/create-socket 8889)
                                :stop (.close tello-command-socket))
@@ -12,13 +17,14 @@
                              :stop (.close tello-state-socket))
 (defstate tello-video-socket :start (drone/create-socket 11111)
                              :stop (.close tello-video-socket))
-(defstate tello-video-stream :start (drone-send "streamon")
-                             :stop (drone-send "streamoff"))
-(defn command [command]
-  (drone/send tello-command-socket (str "command") tello-address 8889)
-  (drone/send tello-command-socket command tello-address 8889))
+(defstate tello-video-stream :start (command "streamon")
+                             :stop (command "streamoff"))
+(defstate udp-listeners :start (start-tello))
 
-(defn parse-state-kv [kv]
+
+(defn parse-state-kv
+  "Converts string 'key:val' provided by tello state to edn map"
+  [kv]
   (let [status-kv (s/split  kv #":")]
    (hash-map (keyword (first status-kv )) (nth status-kv 1))))
 
@@ -33,7 +39,7 @@
 (defn start-tello []
   (try (drone/receive-loop tello-command-socket println))
   (try (drone/receive-loop tello-state-socket update-state!))
-  (drone-send "battery?"))
+  (command "battery?"))
 
 (defn -main
   "fires up the UDP channels"
